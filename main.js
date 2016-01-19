5e7433d9623b002b6d3d040b9226ddf3e97f58bb
Alicia Poindexter <alcampbel@gmail.com>
2016-01-19 10:59:51 -0800

    save questions to firebase

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 'use strict';
 
+var ref = new Firebase('https://aliciafirebaseqna.firebaseio.com/');
+var questionsRef = ref.child('questions');
+var $title, $question, $answer, $questions;
+
 $(document).ready(init);
 
-function init() {}
+function init() {
+  $title = $('#title');
+  $question = $('#question');
+  $answer = $('#answer');
+  $questions =  $('#questions');
+  $('#ask').click(askQuestion);
+}
+
+function askQuestion() {
+  var title = $title.val();
+  var question = $question.val();
+  if(question) {
+    $title.val('');
+    $question.val('');
+    questionsRef.push({
+      title: title,
+      question: question,
+      date: Date.now()
+    })
+  }
+}
